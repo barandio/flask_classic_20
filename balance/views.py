f59b8bd0e41b0c6f7e44e4285006f7488493bd30
Julia B <barandiaranjulia@hotmail.com>
2021-09-27 12:01:45 +0200

    Lista de movimientos desde sqlite

--- balance/views.py
+++ balance/views.py
@@ -1,8 +1,17 @@
+from balance.models import DBManager
 from balance import app
+from flask import render_template
+from balance.models import DBManager
+
+ruta_basedatos = app.config.get("RUTA_BASE_DE_DATOS")
+dbManager = DBManager(ruta_basedatos)
 
 @app.route("/")
 def inicio():
-     return "Página de inicio"
+    
+    movimientos = dbManager.consultaSQL("SELECT * FROM movimientos order by fecha;")
+    
+    return render_template("inicio.html", items=movimientos)
 
 @app.route("/nuevo", methods=("GET", "POST"))
 def nuevo():
